436fda2124bba40e84b9bbd2cf7e23c5cff1b4b3
Eshel <1944549584@qq.com>
2017-10-10 21:09:05 +0800

    加入信鸽推送 SDK

--- currencySpirit/src/main/java/com/eshel/currencyspirit/CurrencySpiritApp.java
+++ currencySpirit/src/main/java/com/eshel/currencyspirit/CurrencySpiritApp.java
@@ -1,6 +1,11 @@
 package com.eshel.currencyspirit;
 
+import com.tencent.android.tpush.XGIOperateCallback;
+import com.tencent.android.tpush.XGPushConfig;
+import com.tencent.android.tpush.XGPushManager;
+
 import baseproject.base.BaseApplication;
+import baseproject.util.Log;
 
 /**
  * createBy Eshel
@@ -14,6 +19,19 @@ public class CurrencySpiritApp extends BaseApplication{
 	public void onCreate() {
 		super.onCreate();
 		app = this;
+		//开启信鸽日志输出
+		XGPushConfig.enableDebug(this, true);
+		//信鸽注册代码
+		XGPushManager.registerPush(this, new XGIOperateCallback() {
+			@Override
+			public void onSuccess(Object data, int flag) {
+				Log.i("TPush", "注册成功，设备token为：" + data);
+			}
+			@Override
+			public void onFail(Object data, int errCode, String msg) {
+				Log.i("TPush", "注册失败，错误码：" + errCode + ",错误信息：" + msg);
+			}
+		});
 	}
 
 	@Override
